e3f8e3d5f3cd85671f74f0ee8e8489f9186ed6ea
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-23 12:05:53 -0700

    rename display divs and half write front-end logic

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Doctor } from './doctor.js';
 
+const displayData = response => {
+  response.data.forEach
+}
 $(document).ready(function() {
-
+  $('#doc-finder').click(e => {
+  $('#doctors').empty
+  e.preventDefault();
+  let userInput = $('doctor-find').val();
+  Doctor.prototype.getDoctors(userInput, displayDoctor)
+});
 });
