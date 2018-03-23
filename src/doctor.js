446cb7d2c11adaa2a7e3979036eeaca7378e82f5
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-23 16:46:30 -0700

    successfully hide api key

--- src/doctor.js
+++ src/doctor.js
@@ -3,7 +3,8 @@ import $ from 'jquery';
 export class Doctor {
 
   getDoctors(userInput, displayDoctor) {
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${userInput}&location=or-portland&skip=0&limit=10&user_key=442323b7f9f43dbf6f83dbf1d4170ff6`)
+    const apiKey = process.env.exports.apiKey;
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${userInput}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`)
     .then(response => {
       displayDoctor(response);
     })
