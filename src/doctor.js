4707148f5543c9960a25cf76fbc6f13b648864d7
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-23 11:20:23 -0700

    write backend logic for grabbing api info

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,12 @@
+import $ from 'jquery';
+
 export class Doctor {
 
-  getDoctors() {
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=portland%2Cor&skip=0&limit=10&user_key=442323b7f9f43dbf6f83dbf1d4170ff6`)
+  getDoctors(userInput, displayDoctor) {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=442323b7f9f43dbf6f83dbf1d4170ff6`)
+    .then(response => {
+      displayDoctor(response);
+    })
+    .fail(() => 'unable to complete')
   }
-
 }
