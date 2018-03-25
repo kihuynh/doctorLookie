c561a3a33ba05618e766bb2f0c1f709b483d099b
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-25 00:10:30 -0700

    add display when there is no match with search

--- src/doctor.js
+++ src/doctor.js
@@ -12,15 +12,4 @@ export class Doctor {
       $('#error').text("Unable to search.")
     });
   }
-
-  // getDisease(medicalIssue, displaySpecials) {
-  //   const apiKey= process.env.exports.apiKey;
-  //   $.get(`https://api.betterdoctor.com/2016-03-01/doctors?&location=or-portland&skip=0&limit=10&user_key=${apiKey}`) NEED TO TAKE AN INPUT FOR MEDICAL ISSUES
-  //   .then(response => {
-  //     displaySpecials(response);
-  //   })
-  //   .fail(() => {
-  //     $('#error').text("Unable to process")
-  //   })
-  // }
 }
--- src/main.js
+++ src/main.js
@@ -4,21 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Doctor } from './doctor.js';
 
 const displayDoctor = response => {
-  response.data.forEach(data => {
-    let firstName = data.profile.first_name;
-    let lastName = data.profile.last_name;
-    let street = data.practices[0].visit_address.street;
-    let state = data.practices[0].visit_address.state;
-    let zip = data.practices[0].visit_address.zip;
-    let phone = data.practices[0].phones[0].number;
-    let website = data.practices[0].website;
-    console.log(data);
-    console.log(data.practices[0].accepts_new_patients);
-    // add website (rarely occurs), accepts if new patients,
+  if (response.data.length == 0 ) {
+    $('#doctors').text("There is no match with your search")
+  } else {
+    response.data.forEach(data => {
+      let firstName = data.profile.first_name;
+      let lastName = data.profile.last_name;
+      let street = data.practices[0].visit_address.street;
+      let state = data.practices[0].visit_address.state;
+      let zip = data.practices[0].visit_address.zip;
+      let phone = data.practices[0].phones[0].number;
+      let website = data.practices[0].website;
+      let patients = data.practices[0].accepts_new_patients;
 
       $('#doctors').append(`<li>`
-        + firstName + " " + lastName + " " + `<br>` + street + `<br>` + state + " " + zip + `<br>` + phone + `<br>` + website + `</li>`)
+        + firstName + " " + lastName + " " + `<br>` + street + `<br>` + state + " " + zip + `<br>` + phone + `<br>` + website + `<br>` + "Accepting new patients?: " + patients + `</li>`)
   })
+  }
 }
 
 
